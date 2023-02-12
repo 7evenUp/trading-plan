efca09b06ae9e86ecc6a30555ad638981e05289a
Artyom <sheludeshev.artyom@mail.ru>
2023-02-12 23:05:27 +0300

    Starting project

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </React.StrictMode>,
+)
---
+++ src/routes/root.tsx
@@ -0,0 +1,9 @@
+const Root = () => {
+  return (
+    <>
+      <h1>Hello, Trading Plan <strong>User</strong></h1>
+    </>
+  )
+}
+
+export default Root

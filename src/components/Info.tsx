eb718a5b017af3f2ca6ca0212b2052d4a2dc77f5
Artyom <sheludeshev.artyom@mail.ru>
2023-02-14 00:37:32 +0300

    Added Info component for showing ui at '/' pathname

---
+++ src/components/Info.tsx
@@ -0,0 +1,14 @@
+
+const Info = () => {
+  return (
+    <div className="p-4 flex-1 flex flex-col gap-4">
+      <h1 className="text-5xl">Welcome to TradingPlan</h1>
+      <div className="w-[570px]">
+        <p className="text-lg">This product helps you with building trading goals, with keeping records of your trades and so on</p>
+        <p className="text-sm text-gray-500 mt-4">I am currently working on product's MVP. So this page will be edited later</p>
+      </div>
+    </div>
+  )
+}
+
+export default Info

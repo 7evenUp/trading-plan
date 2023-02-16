13758b1b70afa19a84ca44f21ed5f78b308479c2
Artyom <sheludeshev.artyom@mail.ru>
2023-02-16 17:19:41 +0300

    Implemented navbar with icons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,49 +1,99 @@
 import { NavLink } from "react-router-dom";
+import { HomeSimple, Presentation, GraphUp, Settings, User } from "iconoir-react";
 
 const Sidebar = () => {
   return (
     <div className="bg-orange-300 p-4">
-      <h1>Sidebar Trade Plan</h1>
       <nav>
         <ul>
           <li>
-            <NavLink
-              to={"plan"}
-              className={({ isActive, isPending }) =>
-                isActive ? "bg-violet-300" : isPending ? "bg-orange-300" : ""
-              }
-            >
-              Plan
+            <NavLink to={"/"} className="flex flex-col items-center">
+              {({ isActive }) => (
+                <>
+                  <div
+                    className={`py-1 px-4 rounded-full ${
+                      isActive
+                        ? "bg-orange-500 hover:bg-orange-600"
+                        : " hover:bg-slate-300"
+                    }`}
+                  >
+                    <HomeSimple />
+                  </div>
+                  <span>Home</span>
+                </>
+              )}
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to={"trades"}
-              className={({ isActive, isPending }) =>
-                isActive ? "bg-violet-300" : isPending ? "bg-orange-300" : ""
-              }
-            >
-              Trades
+            <NavLink to={"plan"} className="flex flex-col items-center">
+              {({ isActive }) => (
+                <>
+                  <div
+                    className={`py-1 px-4 rounded-full ${
+                      isActive
+                        ? "bg-orange-500 hover:bg-orange-600"
+                        : " hover:bg-slate-300"
+                    }`}
+                  >
+                    <Presentation />
+                  </div>
+                  <span>Plan</span>
+                </>
+              )}
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to={"settings"}
-              className={({ isActive, isPending }) =>
-                isActive ? "bg-violet-300" : isPending ? "bg-orange-300" : ""
-              }
-            >
-              Settings
+            <NavLink to={"trades"} className="flex flex-col items-center">
+              {({ isActive }) => (
+                <>
+                  <div
+                    className={`py-1 px-4 rounded-full ${
+                      isActive
+                        ? "bg-orange-500 hover:bg-orange-600"
+                        : " hover:bg-slate-300"
+                    }`}
+                  >
+                    <GraphUp />
+                  </div>
+                  <span>Trades</span>
+                </>
+              )}
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to={"account"}
-              className={({ isActive, isPending }) =>
-                isActive ? "bg-violet-300" : isPending ? "bg-orange-300" : ""
-              }
-            >
-              Account
+            <NavLink to={"settings"} className="flex flex-col items-center">
+              {({ isActive }) => (
+                <>
+                  <div
+                    className={`py-1 px-4 rounded-full ${
+                      isActive
+                        ? "bg-orange-500 hover:bg-orange-600"
+                        : " hover:bg-slate-300"
+                    }`}
+                  >
+                    <Settings />
+                  </div>
+                  <span>Settings</span>
+                </>
+              )}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"account"} className="flex flex-col items-center">
+              {({ isActive }) => (
+                <>
+                  <div
+                    className={`py-1 px-4 rounded-full ${
+                      isActive
+                        ? "bg-orange-500 hover:bg-orange-600"
+                        : " hover:bg-slate-300"
+                    }`}
+                  >
+                    <User />
+                  </div>
+                  <span>Account</span>
+                </>
+              )}
             </NavLink>
           </li>
         </ul>

56d96864c16a220de89751fc6e9d8c11848aa5a1
Artyom <sheludeshev.artyom@mail.ru>
2023-02-19 00:47:14 +0300

    Styled error page

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -1,16 +1,25 @@
 import { useRouteError } from "react-router-dom";
+import { Sidebar } from "./components";
 
 export default function ErrorPage() {
   const error = useRouteError();
-  console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div className="flex w-screen min-h-screen">
+      <Sidebar />
+      <div className="flex flex-1 bg-surfaceVariant min-h-screen items-end">
+        <div className="bg-surface rounded-t-3xl h-[90%] w-5/6 flex flex-col items-center justify-center gap-4">
+          <h1 className="text-4xl text-onSurface">Sorry, some error occured</h1>
+          <div className="flex flex-col gap-2 rounded-xl bg-errorContainer p-6">
+            <p className="text-base tracking-[0.15px] font-medium text-onErrorContainer">
+              Error Status: {error.status} {error.statusText}
+            </p>
+            <p className="text-base tracking-[0.15px] font-medium text-onErrorContainer">
+              {error.data}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

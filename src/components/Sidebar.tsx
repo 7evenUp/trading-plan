08ae780733ff75311efca97da6a94470877b6ef1
Artyom <sheludeshev.artyom@mail.ru>
2023-02-18 21:45:02 +0300

    Styled navbar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,104 +1,80 @@
-import { NavLink } from "react-router-dom";
-import { HomeSimple, Presentation, GraphUp, Settings, User } from "iconoir-react";
+import { NavLink as RouterNavLink } from "react-router-dom";
+import {
+  HomeSimple,
+  Presentation,
+  GraphUp,
+  Settings,
+  User,
+  StatsReport,
+} from "iconoir-react";
 
 const Sidebar = () => {
   return (
-    <div className="bg-surfaceVariant p-4">
+    <div className="bg-surfaceVariant p-3 flex items-center">
       <nav>
-        <ul>
+        <ul className="flex flex-col items-center gap-3 justify-center">
           <li>
-            <NavLink to={"/"} className="flex flex-col items-center">
-              {({ isActive }) => (
-                <>
-                  <div
-                    className={`py-1 px-4 rounded-full ${
-                      isActive
-                        ? "bg-orange-500 hover:bg-orange-600"
-                        : " hover:bg-slate-300"
-                    }`}
-                  >
-                    <HomeSimple />
-                  </div>
-                  <span>Home</span>
-                </>
-              )}
-            </NavLink>
+            <NavLink to="/" link={<HomeSimple />} title="Home" />
           </li>
           <li>
-            <NavLink to={"plan"} className="flex flex-col items-center">
-              {({ isActive }) => (
-                <>
-                  <div
-                    className={`py-1 px-4 rounded-full ${
-                      isActive
-                        ? "bg-orange-500 hover:bg-orange-600"
-                        : " hover:bg-slate-300"
-                    }`}
-                  >
-                    <Presentation />
-                  </div>
-                  <span>Plan</span>
-                </>
-              )}
-            </NavLink>
+            <NavLink to="plan" link={<Presentation />} title="Plan" />
           </li>
           <li>
-            <NavLink to={"trades"} className="flex flex-col items-center">
-              {({ isActive }) => (
-                <>
-                  <div
-                    className={`py-1 px-4 rounded-full ${
-                      isActive
-                        ? "bg-orange-500 hover:bg-orange-600"
-                        : " hover:bg-slate-300"
-                    }`}
-                  >
-                    <GraphUp />
-                  </div>
-                  <span>Trades</span>
-                </>
-              )}
-            </NavLink>
+            <NavLink to="trades" link={<GraphUp />} title="Trades" />
           </li>
           <li>
-            <NavLink to={"settings"} className="flex flex-col items-center">
-              {({ isActive }) => (
-                <>
-                  <div
-                    className={`py-1 px-4 rounded-full ${
-                      isActive
-                        ? "bg-orange-500 hover:bg-orange-600"
-                        : " hover:bg-slate-300"
-                    }`}
-                  >
-                    <Settings />
-                  </div>
-                  <span>Settings</span>
-                </>
-              )}
-            </NavLink>
+            <NavLink to="backtesting" link={<StatsReport />} title="Backtest" />
           </li>
           <li>
-            <NavLink to={"account"} className="flex flex-col items-center">
-              {({ isActive }) => (
-                <>
-                  <div
-                    className={`py-1 px-4 rounded-full ${
-                      isActive
-                        ? "bg-orange-500 hover:bg-orange-600"
-                        : " hover:bg-slate-300"
-                    }`}
-                  >
-                    <User />
-                  </div>
-                  <span>Account</span>
-                </>
-              )}
-            </NavLink>
+            <NavLink to="settings" link={<Settings />} title="Settings" />
+          </li>
+          <li>
+            <NavLink to="account" link={<User />} title="Account" />
           </li>
         </ul>
       </nav>
     </div>
+  );
+};
+
+const NavLink = ({
+  to,
+  link,
+  title,
+}: {
+  to: string;
+  link: React.ReactNode;
+  title: string;
+}) => {
+  return (
+    <RouterNavLink to={to} className="group flex flex-col items-center gap-1">
+      {({ isActive }) => (
+        <>
+          <div
+            className={`py-1 px-4 rounded-full transition-all ${
+              isActive
+                ? "bg-primaryContainer group-hover:bg-primary group-hover:bg-opacity-10 group-active:bg-opacity-20"
+                : "group-hover:bg-onSurfaceVariant group-hover:bg-opacity-10 group-active:bg-opacity-20"
+            }`}
+          >
+            <span
+              className={
+                isActive ? "text-onPrimaryContainer" : "text-onSurfaceVariant"
+              }
+            >
+              {link}
+            </span>
+          </div>
+          <span
+            className={`font-medium text-xs tracking=[0.5px] ${
+              isActive ? "text-onSurface" : "text-onSurfaceVariant"
+            }`}
+          >
+            {title}
+          </span>
+        </>
+      )}
+    </RouterNavLink>
   );
 };
 

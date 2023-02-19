95af92d92ba577ee7041368a612dd87305f93447
Artyom <sheludeshev.artyom@mail.ru>
2023-02-20 02:21:37 +0300

    Started implementing MVP project functions

--- src/routes/planId.tsx
+++ src/routes/planId.tsx
@@ -1,18 +1,21 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { useParams } from "react-router-dom";
 import { SegmentedButton, SegmentedButtonsContainer } from "../components";
 import { useAppSelector } from "../redux/hooks";
 
+const DAYS_IN_WEEK = 7
+
 const PlanID = () => {
   const [activity, setActivity] = useState(4);
   const [duration, setDuration] = useState(182)
+  const totalTradingDays = Math.floor(duration * (activity / DAYS_IN_WEEK))
   const { planId } = useParams();
   const plan = useAppSelector((state) =>
     state.plans.find((plan) => plan.id === planId)
   );
 
-  const onActivityChange = (evt: React.ChangeEvent<HTMLInputElement>) => setActivity(parseInt(evt.target.value))
-  const onDurationChange = (evt: React.ChangeEvent<HTMLInputElement>) => setDuration(parseInt(evt.target.value))
+  const onActivityChange = (evt: ChangeEvent<HTMLInputElement>) => setActivity(parseFloat(evt.target.value))
+  const onDurationChange = (evt: ChangeEvent<HTMLInputElement>) => setDuration(parseFloat(evt.target.value))
 
   return (
     <div className="bg-surface rounded-t-3xl flex-1 flex flex-col p-6">
@@ -25,14 +28,14 @@ const PlanID = () => {
               id="lazy"
               label="Lazy"
               name="activity"
-              value={2.5}
+              value='2.5'
               onChange={onActivityChange}
             />
             <SegmentedButton
               id="active"
               label="Active"
               name="activity"
-              value={4}
+              value='4'
               checked={activity === 4}
               onChange={onActivityChange}
             />
@@ -40,7 +43,7 @@ const PlanID = () => {
               id="full"
               label="Full-time"
               name="activity"
-              value={5.75}
+              value='5.75'
               onChange={onActivityChange}
             />
           </SegmentedButtonsContainer>
@@ -50,21 +53,21 @@ const PlanID = () => {
               id="one"
               label="1"
               name="duration"
-              value={30}
+              value='30'
               onChange={onDurationChange}
             />
             <SegmentedButton
               id="three"
               label="3"
               name="duration"
-              value={91}
+              value='91'
               onChange={onDurationChange}
             />
             <SegmentedButton
               id="six"
               label="6"
               name="duration"
-              value={182}
+              value='182'
               checked={duration === 182}
               onChange={onDurationChange}
             />
@@ -72,21 +75,21 @@ const PlanID = () => {
               id="twelve"
               label="12"
               name="duration"
-              value={365}
+              value='365'
               onChange={onDurationChange}
             />
           </SegmentedButtonsContainer>
 
           <div className="flex flex-col divide-y-0">
-            <div className="flex">
+            <div className="flex justify-between">
               <span>Total trading days per month</span>
-              <span>19 days</span>
+              <span>{totalTradingDays} days</span>
             </div>
-            <div className="flex">
+            <div className="flex justify-between">
               <span>Needed PNL per day</span>
               <span>25.92$</span>
             </div>
-            <div className="flex">
+            <div className="flex justify-between">
               <span>Total successful trades per day</span>
               <div className="flex">
                 <span>19</span>
@@ -94,7 +97,7 @@ const PlanID = () => {
                 <span>4</span>
               </div>
             </div>
-            <div className="flex">
+            <div className="flex justify-between">
               <span>Needed PNL per day</span>
               <div className="flex">
                 <span>2.76$</span>

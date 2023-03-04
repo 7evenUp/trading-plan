bf3a475e21c2a6b464a276ef2ff7070686df8736
Artyom <sheludeshev.artyom@mail.ru>
2023-03-04 18:46:39 +0300

    Removed testing features

--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config'
-
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-    setupFiles: './src/test/setup.ts'
-  },
-})

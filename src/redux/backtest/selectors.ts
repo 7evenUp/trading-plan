b5a3ab4849220786d64290f09177a0f65a82b39a
Artyom <sheludeshev.artyom@mail.ru>
2023-03-09 04:05:32 +0300

    Refactoring selectPNL selector

--- src/redux/backtest/selectors.ts
+++ src/redux/backtest/selectors.ts
@@ -6,42 +6,31 @@ export const selectBacktests = (state: RootState) => state.backtest;
 export const selectPNL = createSelector(
   [selectBacktests, (_, isCompound: boolean) => isCompound],
   (backtest, isCompound) => {
-    if (isCompound) {
-      let pnl = 1;
-      backtest.forEach((item) => {
+    let pnl = 1;
+    backtest.forEach((item) => {
+      if (isCompound) {
         if (item.result === "success") {
-          const percentChange = Math.abs((item.tp - item.entry) / item.entry)
-          pnl *= percentChange + 1;
+          pnl *= Math.abs((item.tp - item.entry) / item.entry) + 1;
         } else {
-          const percentChange = Math.abs((item.sl - item.entry) / item.entry)
-          pnl -= pnl * percentChange;
+          pnl -= Math.abs((item.sl - item.entry) / item.entry) * pnl;
         }
-      });
-  
-      return ((pnl - 1) * 100).toFixed(2);
-    }
-    else {
-      let pnl = 0;
-      backtest.forEach((item) => {
+      } else {
         if (item.result === "success") {
           pnl += Math.abs(((item.tp - item.entry) * 100) / item.entry);
         } else {
           pnl -= Math.abs(((item.sl - item.entry) * 100) / item.entry);
         }
-      });
-  
-      return pnl.toFixed(2);
-    }
+      }
+    });
+
+    return isCompound ? ((pnl - 1) * 100).toFixed(2) : (pnl - 1).toFixed(2);
   }
 );
 
-export const selectWinrate = createSelector(
-  selectBacktests,
-  (backtest) => {
-    const wins = backtest.filter((item) => item.result === "success");
-    return (wins.length * 100) / backtest.length;
-  }
-);
+export const selectWinrate = createSelector(selectBacktests, (backtest) => {
+  const wins = backtest.filter((item) => item.result === "success");
+  return (wins.length * 100) / backtest.length;
+});
 
 export const selectWinsAndFailsAmount = createSelector(
   selectBacktests,
